1c7decdb667488e5cb54f671d54e6e50851b319a
Andrew Yang <yangyuan@users.noreply.github.com>
2025-03-26 11:48:57 -0700

    update logging

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_soloud/flutter_soloud.dart';
+import 'dart:developer' as dev;
+import 'package:logging/logging.dart';
 
 late SoLoud soloud;
 AudioSource? drawCard;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  soloud = SoLoud.instance;
-await soloud.init();
-  drawCard = await soloud.loadAsset(
-      'assets/audio/draw_card.wav',
+
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    // Forward logs to the console.
+    dev.log(
+      record.message,
+      time: record.time,
+      level: record.level.value,
+      name: record.loggerName,
+      zone: record.zone,
+      error: record.error,
+      stackTrace: record.stackTrace,
     );
+  });
+
+  soloud = SoLoud.instance;
+  await soloud.init();
+  drawCard = await soloud.loadAsset('assets/audio/draw_card.wav');
 
   runApp(const MainApp());
 }
