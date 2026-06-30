e40de0ae6f8ece674c74e9965b56160712fe53a5
Andrew Yang <yangyuan@users.noreply.github.com>
2026-06-29 22:01:04 -0700

    Convert MainApp to StatefulWidget with lifecycle
    Refactor MainApp to be a StatefulWidget and add lifecycle management.

--- lib/main.dart
+++ lib/main.dart
@@ -32,8 +32,36 @@ void main() async {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      soloud = SoLoud.instance;
+      soloud.init().then((_) async {
+        drawCard = await soloud.loadAsset('assets/audio/draw_card.wav');
+        drawCardUrl = await soloud.loadUrl('assets/assets/audio/draw_card.wav');
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
